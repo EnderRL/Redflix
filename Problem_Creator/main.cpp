47d7be2aba1ef7873c79e10d497213260fbad604
Juan Miguel De Haro Ruiz <juan.miguel.de.haro@c6s301pc45.fib.upc.es>
2016-12-23 11:17:56 +0100

    Ya va el creador de soluciones

---
+++ Problem_Creator/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+
+typedef list<pair<int, int>>::iterator IteratorP;
+typedef list<int>::iterator IteratorI;
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Usage: Problem_Creator [number of contents] [number of predecessors]" << endl;
+        exit(-1);
+    }
+    int numContents = atoi(argv[1]);
+    int numPredecessors = atoi(argv[2]);
+
+    vector<int> contents(numContents);
+    list<int> listContents;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < numContents; ++i) {
+        listContents.push_back(i);
+    }
+
+    for (int i = 0; i < numContents; ++i) {
+        int random = rand()%listContents.size();
+
+        IteratorI it = listContents.begin();
+        for (int j = 0; j < random; ++j) {
+            ++it;
+        }
+
+        contents[i] = *it;
+        listContents.erase(it);
+    }
+
+    list<pair<int, int>> possibleEdges;
+
+    for (int i = 0; i < numContents; ++i) {
+        for (int j = i+1; j < numContents; ++j) {
+            possibleEdges.push_back(make_pair(contents[i], contents[j]));
+        }
+    }
+
+    cout << "(define (problem basicProblem1)" << endl <<
+            "(:domain redflix)" << endl <<
+            "(:objects";
+    for (int i = 0; i < numContents; ++i) {
+        cout << " c" << i << " - Content";
+    }
+    for (int i = 0; i < numContents; ++i) {
+        cout << " d" << i << " - Day";
+    }
+    cout << ")" << endl << "(:init" << endl;
+   for (int i = 0; i < numPredecessors; ++i) {
+        int random = rand()%possibleEdges.size();
+
+        IteratorP it = possibleEdges.begin();
+        for (int j = 0; j < random; ++j) ++it;
+        pair<int, int>& edge = *it;
+
+        cout << "\t(predecessor c" << edge.first << " c" << edge.second  << ")" << endl;
+
+        possibleEdges.erase(it);
+    }
+    for (int i = 0; i < numContents; ++i) {
+        cout << "\t(= (numDay d" << i << ") " << i << ")" << endl;
+    }
+
+    cout << ")" << endl;
+    cout << " (:goal (forall (?content - Content) (asignedContent ?content)))" << endl << ")" << endl;
+
+}
+
